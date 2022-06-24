2ac89c698d9ca2eb4454515140bc1fe3ee67d39a
sarthaksavvy <sarthak@bitfumes.com>
2022-06-24 19:14:23 +0530

    07 Angular full course 22 - Interpolation

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.css'],
 })
 export class BooksComponent implements OnInit {
+  name: string = 'Clean Code';
+  author: string = 'Robert C Martin';
+  src: string =
+    'https://images-na.ssl-images-amazon.com/images/I/41zoxjP9lcL._SX323_BO1,204,203,200_.jpg';
 
-  constructor() { }
+  name2: string = 'Pragmatic Programmer';
+  author2: string = 'David Thomas';
+  src2: string = 'https://m.media-amazon.com/images/I/518FqJvR9aL.jpg';
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
