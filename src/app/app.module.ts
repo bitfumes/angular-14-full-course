248648bba9500a6a51ab3dd9039b0050df68e262
sarthaksavvy <sarthak@bitfumes.com>
2022-06-24 18:57:51 +0530

    06 Angular full course 22 - Create Component from CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { BooksComponent } from './books/books.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, BooksComponent],
   imports: [BrowserModule],
   bootstrap: [AppComponent],
 })
