216a26cc51b118738a062a6a7e072875f9944304
sarthaksavvy <sarthak@bitfumes.com>
2022-06-24 18:34:22 +0530

    05 Angular full course 22 - Create Root Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
