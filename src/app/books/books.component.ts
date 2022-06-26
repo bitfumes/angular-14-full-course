3227157361a4668ab86cec1fbdd35052bab23580
sarthaksavvy <sarthak@bitfumes.com>
2022-06-26 09:17:25 +0530

    13 Angular full course 22 - ngFor

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,19 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Book {
+  name: string;
+  author: string;
+  image: string;
+}
+
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
   styleUrls: ['./books.component.css'],
 })
 export class BooksComponent implements OnInit {
-  name: string = 'Clean Code';
-  author: string = 'Robert C Martin';
-  src: string =
-    'https://images-na.ssl-images-amazon.com/images/I/41zoxjP9lcL._SX323_BO1,204,203,200_.jpg';
-
-  name2: string = 'Pragmatic Programmer';
-  author2: string = 'David Thomas';
-  src2: string = 'https://m.media-amazon.com/images/I/518FqJvR9aL.jpg';
+  books: Book[] = [
+    {
+      name: 'Clean Code',
+      author: 'Robert C Martin',
+      image:
+        'https://images-na.ssl-images-amazon.com/images/I/41zoxjP9lcL._SX323_BO1,204,203,200_.jpg',
+    },
+    {
+      name: 'Pragmatic Programmer',
+      author: 'David Thomas',
+      image: 'https://m.media-amazon.com/images/I/518FqJvR9aL.jpg',
+    },
+  ];
 
   isShowing: boolean = true;
 
