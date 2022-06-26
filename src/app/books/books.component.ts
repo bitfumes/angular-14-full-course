5eca9ecfa56cfda1cbfed3d37679888e7bfecf65
sarthaksavvy <sarthak@bitfumes.com>
2022-06-26 10:19:04 +0530

    15 Angular full course 22 - style component

--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -27,6 +27,20 @@ export class BooksComponent implements OnInit {
       image: 'https://m.media-amazon.com/images/I/518FqJvR9aL.jpg',
       amount: 800,
     },
+    {
+      name: 'Art of Computer Programming',
+      author: 'Donald John Fuller',
+      image:
+        'https://images-na.ssl-images-amazon.com/images/I/41gCSRxxVeL._SY429_BO1,204,203,200_.jpg',
+      amount: 18300,
+    },
+    {
+      name: 'Introduction to Algorithms',
+      author: 'T Cormen',
+      image:
+        'https://images-na.ssl-images-amazon.com/images/I/41VndKVtiXL._SX442_BO1,204,203,200_.jpg',
+      amount: 1500,
+    },
   ];
 
   isShowing: boolean = true;
