d59acb95ee5a921ee7690f72a43048ffb78671c2
sarthaksavvy <sarthak@bitfumes.com>
2022-06-27 11:16:10 +0530

    26 Angular full course 22 - login register route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { BooksComponent } from './books/books.component';
 import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: BooksComponent },
   { path: 'cart', component: CartComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
 ];
 
 @NgModule({
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule],
+})
+export class AuthModule {}
