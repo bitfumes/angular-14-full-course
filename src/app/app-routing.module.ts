5de5bec0aad446b0984eaaef4930970d4b101e69
sarthaksavvy <sarthak@bitfumes.com>
2022-06-27 13:25:40 +0530

    33 Angular full course 22 - add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { BooksComponent } from './books/books.component';
@@ -7,7 +8,7 @@ import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: BooksComponent },
-  { path: 'cart', component: CartComponent },
+  { path: 'cart', component: CartComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 ];
