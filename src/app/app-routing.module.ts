ed8cf434155ad7fee664cf02ec6ab7e97952d959
sarthaksavvy <sarthak@bitfumes.com>
2022-06-27 10:05:52 +0530

    24 Angular full course 22 - angular routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { CartComponent } from './cart/cart.component';
+
+const routes: Routes = [
+  { path: '', component: BooksComponent },
+  { path: 'cart', component: CartComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BooksModule } from './books/books.module';
 import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [AppComponent, CartComponent],
-  imports: [BrowserModule, FormsModule, BooksModule],
+  imports: [BrowserModule, FormsModule, BooksModule, AppRoutingModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
