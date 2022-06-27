d6da613f686d7425063d6c190f3b39fecc27c3cd
sarthaksavvy <sarthak@bitfumes.com>
2022-06-27 10:29:44 +0530

    25 Angular full course 22 - tailwindcss styling

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -2,11 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BookComponent } from './book/book.component';
 import { BooksComponent } from './books.component';
-import { BooksService } from './books.service';
 
 @NgModule({
   declarations: [BooksComponent, BookComponent],
-  providers: [BooksService],
   imports: [CommonModule],
   exports: [BooksComponent],
 })
